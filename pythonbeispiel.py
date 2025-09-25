5b215d0f0b376a396b3c109b8777feb78dd7dbee
Andreas Salvisberg <andreas.salvisberg@ewb.ch>
2025-09-25 11:53:45 +0000

    beispiel verbesserungen

--- pythonbeispiel.py
+++ pythonbeispiel.py
@@ -4,3 +4,10 @@ print(hostname)
 
 print(hostname.lower())
 hostname.upper
+squares = [x**2 for x in range(5)]
+print(squares)
+
+switch = dict(name="sw01", ip="192.168.0.10", location="Raum10")
+switch.keys()
+
+print(switch.keys())
